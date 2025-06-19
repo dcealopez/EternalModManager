728225b3606a446c975d77439df37267ae67dd2b
dcealopez <dcealopez@gmail.com>
2025-06-19 20:16:59 +0200

    Remove extra parenthesis.

--- EternalModManager/OnlineSafety.cs
+++ EternalModManager/OnlineSafety.cs
@@ -157,8 +157,8 @@ namespace EternalModManager
                 }
 
                 // Allow modification of anything outside of "generated/decls/", except .entities files
-                if ((!modName.StartsWith("generated/decls/", StringComparison.OrdinalIgnoreCase) &&
-                    !modName.StartsWith("decls/", StringComparison.OrdinalIgnoreCase)) &&
+                if (!modName.StartsWith("generated/decls/", StringComparison.OrdinalIgnoreCase) &&
+                    !modName.StartsWith("decls/", StringComparison.OrdinalIgnoreCase) &&
                     !modName.EndsWith(".entities", StringComparison.OrdinalIgnoreCase))
                 {
                     continue;
